f4f5d6ef9e2424034f04fc902fd9b55f5b15463b
LucasTimoteo <95625902+LucasDoGit@users.noreply.github.com>
2023-04-17 20:58:39 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# projeto-loja-virtual
+ Loja virtual desenvolvida em HTML, CSS, JS e Bootstrap como projeto final de bacharel em sistemas de informações.
---
+++ js/main.js
@@ -0,0 +1,41 @@
+var header              = document.getElementById('header');
+var headerNavegation    = document.getElementById('header-navegation');
+var mainContent         = document.getElementById('main-content');
+var showSidebar         = false;
+
+function toggleSidebar()
+{
+    showSidebar = !showSidebar;
+    if(showSidebar)
+    {
+        headerNavegation.style.marginRight = '-50vw';
+        headerNavegation.style.animationName = 'showSidebar';
+        mainContent.style.filter = 'blur(2px)';
+    }
+    else
+    {
+        headerNavegation.style.marginRight = '-150vw';
+        headerNavegation.style.animationName = ''; /*retira animação*/
+        mainContent.style.filter = '';/*retira blur*/
+    }
+}
+
+function closeSidebar()
+{
+    if(showSidebar)
+    {
+        showSidebar = true;
+        toggleSidebar();
+    }
+}
+
+/*fecha o sidebar aumentando a tela*/
+window.addEventListener('resize', function(event) {
+    if(window.innerWidth > 768 && showSidebar) 
+    {  
+        showSidebar = true;
+        toggleSidebar();
+    }
+});
+
+
