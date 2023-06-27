b4dd037c6fe5f8ee91835134e46f8e02850bbd1d
lucastimoteo <95625902+LucasDoGit@users.noreply.github.com>
2023-06-27 17:25:30 -0300

    correção da validação dos campos de registro
    ajustado a validação para envio para a API.

--- public/js/userRegister.js
+++ public/js/userRegister.js
@@ -1,81 +1,83 @@
-const form          = document.getElementById('form-register');
-const campos        = document.querySelectorAll('.required');
-const spans         = document.querySelectorAll('.span-required');
-const emailRegex    = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
-const cpfRegex      = /^[0-9]{3}\.?[0-9]{3}\.?[0-9]{3}\-?[0-9]{2}/;
-const telRegex      = /^(1[1-9]|[4689][0-9]|2[12478]|3([1-5]|[7-8])|5([13-5])|7[193-7])9[0-9]{8}$/;
+const form = document.getElementById('form-register');
+const campos = document.querySelectorAll('.required');
+const spans = document.querySelectorAll('.span-required');
+const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+const cpfRegex = /^[0-9]{3}\.?[0-9]{3}\.?[0-9]{3}\-?[0-9]{2}/;
+const telRegex = /^(1[1-9]|[4689][0-9]|2[12478]|3([1-5]|[7-8])|5([13-5])|7[193-7])9[0-9]{8}$/;
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
 
-    var name = campos[0].value;
-    var cpf = campos[1].value;
-    var age = campos[2].value;
-    var tel = campos[3].value;
-    var email = campos[4].value;
-    var pwd = campos[5].value;
-    var pwd2 = campos[6].value;
+    var name = document.getElementById("idname").value;
+    var cpf = document.getElementById("idcpf").value;
+    var age = document.getElementById("iddt_birth").value;
+    var tel = document.getElementById("idtel").value;
+    var email = document.getElementById("idemail").value;
+    var pwd = document.getElementById("idpassword").value;
+    var pwd2 = document.getElementById("idpassword2").value;
 
-    if (nameValidade(name) && cpfValidade(cpf) && ageValidate(age) && telValidate(tel) 
-        && emailValidate(email) && mainPasswordValidade(pwd) && comparePassword(pwd2)) {
-            console.log("dados enviados");
-            sendAPI();
+    if (name && cpf && age && tel && email && pwd && pwd2) {
+        // Campos estão corretos, enviar para o backend
+        sendAPI(name, cpf, age, tel, email, pwd);
     } else {
-        nameValidade()
+        nameValidade();
         cpfValidade();
         ageValidate();
         telValidate();
         emailValidate();
         mainPasswordValidade();
         comparePassword();
-        console.log("dados não enviados");
     }
 });
 
 function setError(index) {
     campos[index].style.border = '2px solid #e63636'
-    spans[index].style.display = 'block';
+    spans[index].style.display = 'block'
+    document.getElementById("btn-submit").disabled = true;
+    document.getElementById("btn-submit").style.opacity = '50%'
 }
 
 function removeError(index) {
     campos[index].style.border = ''
     spans[index].style.display = 'none';
+    document.getElementById("btn-submit").disabled = false;
+    document.getElementById("btn-submit").style.opacity = '100%'
 }
 
 function nameValidade() {
-    if(campos[0].value.length < 3) {
+    if (campos[0].value.length < 3) {
         setError(0);
     } else {
         removeError(0);
     }
 }
 
-function cpfValidade(){
-    if(!cpfRegex.test(campos[1].value)){
+function cpfValidade() {
+    if (!cpfRegex.test(campos[1].value)) {
         setError(1);
     } else {
         removeError(1);
     }
 }
 
-function ageValidate() {       
-        var dt_birth = new Date(campos[2].value);
-        var today = new Date();
-        
-        var diff = today - dt_birth;
-        var age = Math.floor(diff / (1000 * 60 * 60 * 24 * 365.25)); // Calcula a idade em anos
-        
-        if (age >= 18) {
-          // O usuário tem 18 anos ou mais, pode prosseguir com o envio do formulário
-          removeError(2);
-        } else {
-          // O usuário é menor de 18 anos, exiba uma mensagem de erro ou realize outra ação apropriada
-          setError(2);
-        }
+function ageValidate() {
+    var dt_birth = new Date(campos[2].value);
+    var today = new Date();
+
+    var diff = today - dt_birth;
+    var age = Math.floor(diff / (1000 * 60 * 60 * 24 * 365.25)); // Calcula a idade em anos
+
+    if (age >= 18) {
+        // O usuário tem 18 anos ou mais, pode prosseguir com o envio do formulário
+        removeError(2);
+    } else {
+        // O usuário é menor de 18 anos, exiba uma mensagem de erro ou realize outra ação apropriada
+        setError(2);
+    }
 }
 
 function telValidate() {
-    if(!telRegex.test(campos[3].value)){
+    if (!telRegex.test(campos[3].value)) {
         setError(3);
     } else {
         removeError(3);
@@ -83,7 +85,7 @@ function telValidate() {
 }
 
 function emailValidate() {
-    if(!emailRegex.test(campos[4].value)){
+    if (!emailRegex.test(campos[4].value)) {
         setError(4);
     } else {
         removeError(4);
@@ -91,7 +93,7 @@ function emailValidate() {
 }
 
 function mainPasswordValidade() {
-    if(campos[5].value.length < 8) {
+    if (campos[5].value.length < 8) {
         setError(5);
     } else {
         removeError(5);
@@ -99,14 +101,42 @@ function mainPasswordValidade() {
     }
 }
 
-function comparePassword(){
-    if(campos[5].value == campos[6].value && campos[6].value.length >= 8){
+function comparePassword() {
+    if (campos[5].value == campos[6].value && campos[6].value.length >= 8) {
         removeError(6);
     } else {
         setError(6)
     }
 }
 
-function sendAPI() {
-    console.log('dados enviados');
+function sendAPI(name, cpf, age, tel, email, pwd) {
+
+    var formData = new URLSearchParams();
+    formData.append("name", name);
+    formData.append("cpf", cpf);
+    formData.append("dt_birth", age);
+    formData.append("tel", tel);
+    formData.append("email", email);
+    formData.append("password", pwd);
+
+    fetch("http://localhost:3000/api/auth/register", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        body: formData.toString()
+    })
+        .then(function (response) {
+            if (response.ok) {
+                // envio bem-sucedido
+                alert("Dados enviados com sucesso!");
+                window.location.href = "http://localhost:3000/index.html";
+            } else {
+                throw new Error("Erro ao enviar dados para o backend.");
+            }
+        })
+        .catch(function (error) {
+            console.error(error);
+            alert("Ocorreu um erro ao enviar os dados para o backend.");
+        });
 }
