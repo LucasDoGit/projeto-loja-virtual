5cd7337eb2726e7b890fb95eb66ca16e7569ca2a
Brunnonno <brunonaiser@hotmail.com>
2023-12-02 20:29:43 -0300

    Adição de Slider
    Adição de Slider

--- front/js/slider.js
+++ front/js/slider.js
@@ -1,33 +1,33 @@
 $(document).ready(function () {
-    var carousel = $("#carousel");
-    var productWidth = $(".product").outerWidth(true); // Inclui margens
+    var carousel = $("#produtosPromocao");
+    var productWidth = $(".card-produtot").outerWidth(true); // Inclui margens
 
     function scrollCarousel() {
         carousel.animate({ "margin-left": -productWidth }, 500, function () {
             // Mova o primeiro item para o final após a animação
-            $(this).append($(".product:first").detach()).css({ "margin-left": 0 });
+            $(this).append($(".card-produto:first").detach()).css({ "margin-left": 0 });
         });
     }
 
     // Adiciona a funcionalidade de rotação automática
-    var interval = setInterval(scrollCarousel, 2000); // Ajuste o intervalo conforme necessário
+    var interval = setInterval(scrollCarousel, 5000); // Ajuste o intervalo conforme necessário
 
     // Pausar a rotação ao passar o mouse sobre o carrossel
-    $("#carousel-container").hover(function () {
+    $("#produtosPromocao").hover(function () {
         clearInterval(interval);
     }, function () {
-        interval = setInterval(scrollCarousel, 2000);
+        interval = setInterval(scrollCarousel, 5000);
     });
 
     // Adiciona funcionalidade aos botões de navegação
     $("#prev-button").click(function () {
-        carousel.prepend($(".product:last").detach()).css({ "margin-left": -productWidth });
+        carousel.prepend($(".card-produto:last").detach()).css({ "margin-left": -productWidth });
         carousel.animate({ "margin-left": 0 }, 500);
     });
 
     $("#next-button").click(function () {
         carousel.animate({ "margin-left": -productWidth }, 500, function () {
-            $(this).append($(".product:first").detach()).css({ "margin-left": 0 });
+            $(this).append($(".card-produto:first").detach()).css({ "margin-left": 0 });
         });
     });
 });
