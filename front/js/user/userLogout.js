a8973fc06951eb59e04ec984b11192ca187b7065
lucastimoteo <95625902+LucasDoGit@users.noreply.github.com>
2023-12-05 13:45:50 -0300

    ajuste na tela do carrinho
    ao acessar a página sem login era direcionado para localhost. Ajustado uma pequena visualização em telas menores

--- front/js/user/userLogout.js
+++ front/js/user/userLogout.js
@@ -1,6 +1,6 @@
 if(localStorage.getItem('token') == null) {
     alert("Você precisa estar logado para acessar essa página!");
-    window.location.href = 'http://localhost:3000/front/pages/login.html'
+    window.location.href = '/front/pages/login.html'
 }
 
 function sair() {
